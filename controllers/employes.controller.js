fded57605442a73d3a05a4df3fdda20271e1e340
unknown <jonathanpoblet228@gmail.com>
2024-07-04 10:43:50 -0300

    Correct res.json of Employes

--- controllers/employes.controller.js
+++ controllers/employes.controller.js
@@ -3,7 +3,7 @@ import { employesService } from '../services/employes.service.js';
 export async function controllerGetEmployes(req, res, next) {
   try {
     const employes = await employesService.getAll();
-    res.json({ employes });
+    res.json(employes);
   } catch (error) {
     res.status(500).send('Internal Server Error');
   }
