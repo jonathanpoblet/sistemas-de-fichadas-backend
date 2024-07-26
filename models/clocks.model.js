301a6af3aba56f272ec1d80ee1e71fe9ca81647a
unknown <jonathanpoblet228@gmail.com>
2024-07-26 15:24:47 -0300

    Added Company in GetAllClocks

--- models/clocks.model.js
+++ models/clocks.model.js
@@ -13,10 +13,23 @@ export class ClocksModel {
 
   async getAll() {
     try {
-      const query = await this.client(this.table).select('*');
+      const query = await this.client(this.table)
+        .select(
+          'clocks.*',
+          this.client.raw("CONCAT(employes.name, ' ', employes.lastname) AS employe"),
+          this.client.raw(
+            "DATE_FORMAT(CONVERT_TZ(clocks.date, '+00:00', '-00:00'), '%Y-%m-%d %H:%i:%s') AS date"
+          ),
+          'offices.branch',
+          'companies.company'
+        )
+        .join('employes', 'employes.id_employe', 'clocks.id_employe')
+        .join('offices', 'offices.id_office', 'clocks.id_office')
+        .join('companies', 'clocks.id_office', 'companies.id_company')
+        .orderByRaw("CONVERT_TZ(clocks.date, '+00:00', '-00:00') DESC");
       return query;
     } catch (error) {
-      throw new Error(`Can not get all: ${error}`);
+      throw new Error(`Can not get all: ${error.message}`);
     }
   }
 
